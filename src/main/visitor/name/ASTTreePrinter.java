300164f598ff823cb14dbf125caf486234c52a21
Sharif <s.mhashemi.221@gmail.com>
2022-04-20 14:27:08 +0430

    feat(ASTTreePrinter): Add classDeclaration.

--- src/main/visitor/name/ASTTreePrinter.java
+++ src/main/visitor/name/ASTTreePrinter.java
@@ -1,8 +1,11 @@
 package main.visitor.name;
 
 import main.ast.nodes.*;
-import main.ast.nodes.declaration.*;
-import main.ast.nodes.declaration.struct.*;
+import main.ast.nodes.declaration.classDec.ClassDeclaration;
+import main.ast.nodes.declaration.classDec.classMembersDec.ConstructorDeclaration;
+import main.ast.nodes.declaration.classDec.classMembersDec.FieldDeclaration;
+import main.ast.nodes.declaration.classDec.classMembersDec.MethodDeclaration;
+import main.ast.nodes.declaration.variableDec.VariableDeclaration;
 import main.ast.nodes.expression.*;
 import main.ast.nodes.expression.values.primitive.*;
 import main.ast.nodes.statement.*;
@@ -16,64 +19,92 @@ public class ASTTreePrinter extends Visitor<Void> {
     @Override
     public Void visit(Program program) {
         messagePrinter(program.getLine(), program.toString());
-        for (StructDeclaration structDeclaration: program.getStructs())
-            structDeclaration.accept(this);
-        for (FunctionDeclaration functionDeclaration:program.getFunctions())
-            functionDeclaration.accept(this);
-        program.getMain().accept(this);
+        for (ClassDeclaration classDeclaration: program.getClasses())
+            classDeclaration.accept(this);
+//        program.getMain().accept(this); //todo
         return null;
     }
 
-    @Override
-    public Void visit(FunctionDeclaration functionDec) {
-        messagePrinter(functionDec.getLine(), functionDec.toString());
-        functionDec.getFunctionName().accept(this);
-        for (VariableDeclaration var: functionDec.getArgs())
-            var.accept(this);
-        functionDec.getBody().accept(this);
-        return null;
-    }
-
-    @Override
-    public Void visit(MainDeclaration mainDec) {
-        messagePrinter(mainDec.getLine(), mainDec.toString());
-        mainDec.getBody().accept(this);
-        return null;
-    }
+//    @Override //todo
+//    public Void visit(MainDeclaration mainDec) {
+//        messagePrinter(mainDec.getLine(), mainDec.toString());
+//        mainDec.getBody().accept(this);
+//        return null;
+//    }
 
     @Override
     public Void visit(VariableDeclaration variableDec) {
         messagePrinter(variableDec.getLine(), variableDec.toString());
         variableDec.getVarName().accept(this);
-        if (variableDec.getDefaultValue() != null)
-            variableDec.getDefaultValue().accept(this);
+//        if (variableDec.getDefaultValue() != null) //todo
+//            variableDec.getDefaultValue().accept(this);
         return null;
     }
 
     @Override
-    public Void visit(StructDeclaration structDec) {
-        messagePrinter(structDec.getLine(), structDec.toString());
-        structDec.getStructName().accept(this);
-        structDec.getBody().accept(this);
+    public Void visit(ClassDeclaration classDec) {
+        messagePrinter(classDec.getLine(), classDec.toString());
+        classDec.getClassName().accept(this);
+        if(classDec.getParentClassName() != null) {
+            classDec.getParentClassName().accept(this);
+        }
+        for(FieldDeclaration fieldDeclaration : classDec.getFields()) {
+            fieldDeclaration.accept(this);
+        }
+        if(classDec.getConstructor() != null) {
+            classDec.getConstructor().accept(this);
+        }
+        for(MethodDeclaration methodDeclaration : classDec.getMethods()) {
+            methodDeclaration.accept(this);
+        }
         return null;
     }
 
     @Override
-    public Void visit(SetGetVarDeclaration setGetVarDec) {
-        messagePrinter(setGetVarDec.getLine(), setGetVarDec.toString());
-        setGetVarDec.getVarName().accept(this);
-        for (VariableDeclaration var: setGetVarDec.getArgs())
-            var.accept(this);
-        setGetVarDec.getSetterBody().accept(this);
-        setGetVarDec.getGetterBody().accept(this);
+    public Void visit(ConstructorDeclaration constructorDec) {
+        messagePrinter(constructorDec.getLine(), constructorDec.toString());
+        constructorDec.getMethodName().accept(this);
+        for(VariableDeclaration varDeclaration : constructorDec.getArgs()) {
+            varDeclaration.accept(this);
+        }
+        for(VariableDeclaration varDeclaration : constructorDec.getLocalVars()) {
+            varDeclaration.accept(this);
+        }
+        for(Statement statement : constructorDec.getBody()) {
+            statement.accept(this);
+        }
         return null;
     }
+
+    @Override
+    public Void visit(MethodDeclaration methodDec) {
+        messagePrinter(methodDec.getLine(), methodDec.toString());
+        methodDec.getMethodName().accept(this);
+        for(VariableDeclaration varDeclaration : methodDec.getArgs()) {
+            varDeclaration.accept(this);
+        }
+        for(VariableDeclaration varDeclaration : methodDec.getLocalVars()) {
+            varDeclaration.accept(this);
+        }
+        for(Statement statement : methodDec.getBody()) {
+            statement.accept(this);
+        }
+        return null;
+    }
+
+    @Override
+    public Void visit(FieldDeclaration fieldDec) {
+        messagePrinter(fieldDec.getLine(), fieldDec.toString());
+        fieldDec.getVarDeclaration().accept(this);
+        return null;
+    }
+
 
     @Override
     public Void visit(AssignmentStmt assignmentStmt) {
         messagePrinter(assignmentStmt.getLine(), assignmentStmt.toString());
-        assignmentStmt.getLValue().accept(this);
-        assignmentStmt.getRValue().accept(this);
+        assignmentStmt.getlValue().accept(this);
+        assignmentStmt.getrValue().accept(this);
         return null;
     }
 
@@ -96,16 +127,16 @@ public class ASTTreePrinter extends Visitor<Void> {
     }
 
     @Override
-    public Void visit(FunctionCallStmt functionCallStmt) {
-        messagePrinter(functionCallStmt.getLine(), functionCallStmt.toString());
-        functionCallStmt.getFunctionCall().accept(this);
+    public Void visit(MethodCallStmt methodCallStmt) {
+        messagePrinter(methodCallStmt.getLine(), methodCallStmt.toString());
+        methodCallStmt.getMethodCall().accept(this);
         return null;
     }
 
     @Override
-    public Void visit(DisplayStmt displayStmt) {
-        messagePrinter(displayStmt.getLine(), displayStmt.toString());
-        displayStmt.getArg().accept(this);
+    public Void visit(PrintStmt print) {
+        messagePrinter(print.getLine(), print.toString());
+        print.getArg().accept(this);
         return null;
     }
 
@@ -117,35 +148,35 @@ public class ASTTreePrinter extends Visitor<Void> {
         return null;
     }
 
-    @Override
-    public Void visit(LoopStmt loopStmt) {
-        messagePrinter(loopStmt.getLine(), loopStmt.toString());
-        loopStmt.getCondition().accept(this);
-        loopStmt.getBody().accept(this);
-        return null;
-    }
+//    @Override //todo
+//    public Void visit(LoopStmt loopStmt) {
+//        messagePrinter(loopStmt.getLine(), loopStmt.toString());
+//        loopStmt.getCondition().accept(this);
+//        loopStmt.getBody().accept(this);
+//        return null;
+//    }
 
-    @Override
-    public Void visit(VarDecStmt varDecStmt) {
-        messagePrinter(varDecStmt.getLine(), varDecStmt.toString());
-        for (VariableDeclaration var: varDecStmt.getVars())
-            var.accept(this);
-        return null;
-    }
+//    @Override //todo
+//    public Void visit(VarDecStmt varDecStmt) {
+//        messagePrinter(varDecStmt.getLine(), varDecStmt.toString());
+//        for (VariableDeclaration var: varDecStmt.getVars())
+//            var.accept(this);
+//        return null;
+//    }
 
-    @Override
-    public Void visit(ListAppendStmt listAppendStmt) {
-        messagePrinter(listAppendStmt.getLine(), listAppendStmt.toString());
-        listAppendStmt.getListAppendExpr().accept(this);
-        return null;
-    }
+//    @Override //todo
+//    public Void visit(ListAppendStmt listAppendStmt) {
+//        messagePrinter(listAppendStmt.getLine(), listAppendStmt.toString());
+//        listAppendStmt.getListAppendExpr().accept(this);
+//        return null;
+//    }
 
-    @Override
-    public Void visit(ListSizeStmt listSizeStmt) {
-        messagePrinter(listSizeStmt.getLine(), listSizeStmt.toString());
-        listSizeStmt.getListSizeExpr().accept(this);
-        return null;
-    }
+//    @Override //todo
+//    public Void visit(ListSizeStmt listSizeStmt) {
+//        messagePrinter(listSizeStmt.getLine(), listSizeStmt.toString());
+//        listSizeStmt.getListSizeExpr().accept(this);
+//        return null;
+//    }
 
     @Override
     public Void visit(BinaryExpression binaryExpression) {
@@ -163,11 +194,12 @@ public class ASTTreePrinter extends Visitor<Void> {
     }
 
     @Override
-    public Void visit(FunctionCall funcCall) {
-        messagePrinter(funcCall.getLine(), funcCall.toString());
-        funcCall.getInstance().accept(this);
-        for (Expression expr: funcCall.getArgs())
-            expr.accept(this);
+    public Void visit(MethodCall methodCall) {
+        messagePrinter(methodCall.getLine(), methodCall.toString());
+        methodCall.getInstance().accept(this);
+        for(Expression expression : methodCall.getArgs()) {
+            expression.accept(this);
+        }
         return null;
     }
 
@@ -185,36 +217,28 @@ public class ASTTreePrinter extends Visitor<Void> {
         return null;
     }
 
-    @Override
-    public Void visit(StructAccess structAccess) {
-        messagePrinter(structAccess.getLine(), structAccess.toString());
-        structAccess.getInstance().accept(this);
-        structAccess.getElement().accept(this);
-        return null;
-    }
-
-    @Override
-    public Void visit(ListSize listSize) {
-        messagePrinter(listSize.getLine(), listSize.toString());
-        listSize.getArg().accept(this);
-        return null;
-    }
-
-    @Override
-    public Void visit(ListAppend listAppend) {
-        messagePrinter(listAppend.getLine(), listAppend.toString());
-        listAppend.getListArg().accept(this);
-        listAppend.getElementArg().accept(this);
-        return null;
-    }
-
-    @Override
-    public Void visit(ExprInPar exprInPar) {
-        messagePrinter(exprInPar.getLine(), exprInPar.toString());
-        for (Expression expr: exprInPar.getInputs())
-            expr.accept(this);
-        return null;
-    }
+//    @Override //todo
+//    public Void visit(ListSize listSize) {
+//        messagePrinter(listSize.getLine(), listSize.toString());
+//        listSize.getArg().accept(this);
+//        return null;
+//    }
+//
+//    @Override //todo
+//    public Void visit(ListAppend listAppend) {
+//        messagePrinter(listAppend.getLine(), listAppend.toString());
+//        listAppend.getListArg().accept(this);
+//        listAppend.getElementArg().accept(this);
+//        return null;
+//    }
+//
+//    @Override //todo
+//    public Void visit(ExprInPar exprInPar) {
+//        messagePrinter(exprInPar.getLine(), exprInPar.toString());
+//        for (Expression expr: exprInPar.getInputs())
+//            expr.accept(this);
+//        return null;
+//    }
 
     @Override
     public Void visit(IntValue intValue) {
