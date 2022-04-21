0cd50e153359ef6dc95a53657d004c609d5ab092
Sharif <s.mhashemi.221@gmail.com>
2022-04-21 11:27:05 +0430

    feat(errors): Add CyclicDependency.

--- src/main/compileError/nameError/CyclicDependency.java
+++ src/main/compileError/nameError/CyclicDependency.java
@@ -3,7 +3,7 @@ package main.compileError.nameError;
 import main.compileError.CompileError;
 
 public class CyclicDependency extends CompileError {
-    public CyclicDependency(int line, String structName) {
-        super(line, "Struct " + structName + " is in dependency cycle");
+    public CyclicDependency(int line, String className) {
+        super(line, "Class " + className + " is in a dependency cycle");
     }
 }
