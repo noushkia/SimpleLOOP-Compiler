c0c70a55bbca2663f8f7dea7155c95f081c2d96f
kianoush <kianoosharshi@gmail.com>
2022-04-23 10:39:12 +0430

    Remove cyclic dependency error

--- src/main/compileError/nameError/CyclicDependency.java
+++
@@ -1,9 +0,0 @@
-package main.compileError.nameError;
-
-import main.compileError.CompileError;
-
-public class CyclicDependency extends CompileError {
-    public CyclicDependency(int line, String className) {
-        super(line, "Class " + className + " is in a dependency cycle");
-    }
-}
