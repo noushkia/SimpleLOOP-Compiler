257dfc0d6f7457a43c5d8e575bcb64e99aef4562
kianoush <kianoosharshi@gmail.com>
2022-04-27 08:24:53 +0430

    Add some nameAnalyzer methods

--- src/main/visitor/name/NameAnalyzer.java
+++ src/main/visitor/name/NameAnalyzer.java
@@ -2,6 +2,8 @@ package main.visitor.name;
 
 import main.ast.nodes.Program;
 import main.ast.nodes.declaration.classDec.ClassDeclaration;
+import main.ast.nodes.declaration.classDec.classMembersDec.ConstructorDeclaration;
+import main.ast.nodes.declaration.classDec.classMembersDec.FieldDeclaration;
 import main.ast.nodes.declaration.classDec.classMembersDec.MethodDeclaration;
 import main.ast.nodes.declaration.variableDec.VariableDeclaration;
 import main.ast.nodes.statement.*;
@@ -14,12 +16,35 @@ public class NameAnalyzer extends Visitor<Void> {
         SymbolTable root = new SymbolTable();
         SymbolTable.root = root;
         SymbolTable.push(root);
-        //..............
+        // visit classes and global variables
+        // add hierarchy
+        return null;
+    }
+
+    @Override
+    public Void visit(ClassDeclaration classDeclaration) {
+        if(classDeclaration.getParentClassName() != null) {
+//            if (this.classHierarchy.isSecondNodeAncestorOf(classDeclaration.getParentClassName().getName(), classDeclaration.getClassName().getName())) {
+//                ClassInCyclicInheritance exception = new ClassInCyclicInheritance(classDeclaration);
+//                classDeclaration.addError(exception);
+//            }
+        }
+        for(FieldDeclaration fieldDeclaration : classDeclaration.getFields()) {
+            fieldDeclaration.accept(this);
+        }
+        if(classDeclaration.getConstructor() != null) {
+            classDeclaration.getConstructor().accept(this);
+        }
+        for(MethodDeclaration methodDeclaration : classDeclaration.getMethods()) {
+            methodDeclaration.accept(this);
+        }
         return null;
     }
 
     @Override
     public Void visit(MethodDeclaration methodDeclaration) {
+        //check conflict with fields and other methods
+        // use Symbol Table
         for (VariableDeclaration arg: methodDeclaration.getArgs())
             arg.accept(this);
         for (VariableDeclaration var: methodDeclaration.getLocalVars())
@@ -30,32 +55,66 @@ public class NameAnalyzer extends Visitor<Void> {
     }
 
     @Override
-    public Void visit(ClassDeclaration classDeclaration) {
+    public Void visit(ConstructorDeclaration constructorDeclaration) {
+        this.visit((MethodDeclaration) constructorDeclaration);
         return null;
     }
 
     @Override
+    public Void visit(FieldDeclaration fieldDeclaration) {
+        // check field conflicts
+        return null;
+    }
+
+    @Override
+    //todo: check
     public Void visit(BlockStmt blockStmt) {
+        for (Statement stmt : blockStmt.getStatements()) {
+            stmt.accept(this);
+        }
         return null;
     }
 
     @Override
     public Void visit(ConditionalStmt conditionalStmt) {
+        SymbolTable ifScope = new SymbolTable(SymbolTable.top);
+        SymbolTable.push(ifScope);
+        conditionalStmt.getThenBody().accept(this);
+        SymbolTable.pop();
+
+        for (ElsifStmt elsifStmt: conditionalStmt.getElsif())
+            elsifStmt.accept(this);
+
+        if (conditionalStmt.getElseBody() != null) {
+            SymbolTable elseScope = new SymbolTable(SymbolTable.top);
+            SymbolTable.push(elseScope);
+            conditionalStmt.getElseBody().accept(this);
+            SymbolTable.pop();
+        }
         return null;
     }
 
     @Override
     public Void visit(ElsifStmt elsifStmt) {
+        SymbolTable elsifScope = new SymbolTable(SymbolTable.top);
+        SymbolTable.push(elsifScope);
+        elsifStmt.getThenBody().accept(this);
+        SymbolTable.pop();
         return null;
     }
 
     @Override
     public Void visit(EachStmt eachStmt) {
+        SymbolTable loopScope = new SymbolTable(SymbolTable.top);
+        SymbolTable.push(loopScope);
+        eachStmt.getBody().accept(this);
+        SymbolTable.pop();
         return null;
     }
 
     @Override
     public Void visit(VariableDeclaration variableDeclaration) {
+        //
         return null;
     }
 }
